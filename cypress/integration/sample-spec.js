32fc9f9a05d44003e83ac7bb28a8c1a5666d9a50
Mikkel <mikkelandersen21@gmail.com>
2019-05-05 23:32:40 +0200

    add code.

--- cypress/integration/sample-spec.js
+++ cypress/integration/sample-spec.js
@@ -1,9 +1,25 @@
-describe('DFDS booking flow', function() {
-    beforeEach(function() {
-        cy.visit('https://ferry.dfdsseaways.com/');
-      });
-    
-    it('contains "Ferry booking" in the title', function() {
-        cy.title().should('contain', 'Ferry booking');
-    });
+describe("DFDS booking flow", function() {
+  before(function() {});
+  
+  it("It should open page", function() {
+    cy.visit("https://ferry.dfdsseaways.com/");  
+  });
+
+  it("It should test travel step", function() {
+    cy.get(".travel-container").should("be.visible");
+    cy.get(".flow-container.is-loading.active").should("not.exist");
+    cy.get("#SubmitContinue").click();
+  });
+
+  it("It should test cabin-fares step", function() {
+    cy.get(".cabin-fares-container").should("be.visible");
+    cy.get(".flow-container.is-loading.active").should("not.exist");
+    cy.get("#SubmitContinue").click();
+  });
+
+  it("It should test meals step", function() {
+    cy.get(".meals-container").should("be.visible");
+    cy.get(".flow-container.is-loading.active").should("not.exist");
+    cy.get("#SubmitContinue").click();
+  });
 });
